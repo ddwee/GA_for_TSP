9929ea5944c9b38ef001dcc5034f43b52e8cc391
ddwee <103949785+ddwee@users.noreply.github.com>
2022-10-02 10:42:40 +0900

    まともに動くようにした

--- PfGA.py
+++ PfGA.py
@@ -2,6 +2,7 @@ import random
 import math
 import csv
 import copy
+import time
 
 
 
@@ -76,48 +77,56 @@ class Route:
                                        (cities[self.citynums[i-1]].X,
                                         cities[self.citynums[i-1]].Y))
         return self.distance
-    
-    
-    def copy_route(self,route):
-        return copy.copy(route)  # deepcopy?
-    
-    
-    def crossover(self, p1, p2):
-        # 子の遺伝子情報
-        c1 = self.copy_route(p1)
-        c2 = self.copy_route(p2)
-        for i in range(CITIES_N):
-            if random.random() > 0.5:
-                c1[i], c2[i] = c2[i], c1[i]
-                
-        mutated_c1, mutated_c2 = self.mutate(c1,c2)
-        
-        return mutated_c1,mutated_c2
-   
-    
-    # 一様交叉
-    def mutate(self,c1,c2,mutate_rate=0.05):
-        if random.random() > mutate_rate:
-            if random.random() > 0.5:
-                select_num = [i for i in range(c1)]
-                select_index = random.sample(select_num, 2)
-                
-                a = c1[select_index[0]]
-                b = c1[select_index[1]]
-                c1[select_index[1]] = a
-                c1[select_index[0]] = b
 
-            else:
-                select_num = [i for i in range(c2)]
-                select_index = random.sample(select_num, 2)
-                
-                a = c2[select_index[0]]
-                b = c2[select_index[1]]
-                c2[select_index[1]] = a
-                c2[select_index[0]] = b
+
+def copy_route(route):
+    return copy.deepcopy(route)
+
+
+def mutate(c1,c2):
+    if random.random() > 0.5:
+        select_num = [i for i in range(len(c1.citynums))]
+        select_index = random.sample(select_num, 2)
         
-        return c1,c2
+        a = c1.citynums[select_index[0]]
+        b = c1.citynums[select_index[1]]
+        c1.citynums[select_index[1]] = a
+        c1.citynums[select_index[0]] = b
+
+    else:
+        select_num = [i for i in range(len(c2.citynums))]
+        select_index = random.sample(select_num, 2)
+        
+        a = c2.citynums[select_index[0]]
+        b = c2.citynums[select_index[1]]
+        c2.citynums[select_index[1]] = a
+        c2.citynums[select_index[0]] = b
+
+    return c1,c2
+
+
+
+def crossover(p1, p2):
+    # 子の遺伝子情報
+    c1 = copy_route(p1)
+    c2 = copy_route(p2)
+    # 切り離す位置をランダムに選択
+    index = random.randint(1,len(cities_data)-2)
+    # indexの前までは自身の経路
+    #indexの後からは相方のリスト(index前の都市と重複しないように)
+    fragment_c1 = c1.citynums[:index]
+    fragment_c2 = c2.citynums[:index]
     
+    notinslice_c1 = [X for X in fragment_c2 if X not in c1.citynums]
+    notinslice_c2 = [X for X in fragment_c1 if X not in c2.citynums]
+    #リストを合体
+    c1.citynums += notinslice_c1
+    c2.citynums += notinslice_c2
+    
+    mutated_c1, mutated_c2 = mutate(c1,c2)
+    
+    return mutated_c1,mutated_c2
+
 
 def pfga():
 
@@ -130,49 +139,50 @@ def pfga():
     p2 = population.pop(random.randint(0, len(population)-1))
 
     # 子を作成
-    r = Route()
-    c1, c2 = r.crossover(p1,p2)
+    c1, c2 = crossover(p1,p2)
 
     if p1.calc_distance() < p2.calc_distance():
-        p_min = p1
-        p_max = p2
+        p_good = p1  # 短い経路(優秀)
+        p_bad = p2  # 長い経路(淘汰される)
     else:
-        p_min = p2
-        p_max = p1
+        p_good = p2
+        p_bad = p1
     if c1.calc_distance() < c2.calc_distance():
-        c_min = c1
-        c_max = c2
+        c_good = c1
+        c_bad = c2
     else:
-        c_min = c2
-        c_max = c1
+        c_good = c2
+        c_bad = c1
 
-    if c_min.calc_distance() >= p_max.calc_distance():
+    if c_bad.calc_distance() <= p_good.calc_distance():
         # 子2個体がともに親の2個体より良かった場合
         # 子2個体及び適応度の良かった方の親個体計3個体が局所集団に戻り、局所集団数は1増加する。
         population.append(c1)
         population.append(c2)
-        population.append(p_max)
-    elif p_min.calc_distance() >= c_max.calc_distsnce():
+        population.append(p_good)
+    elif p_bad.calc_distance() <= c_good.calc_distance():
         # 子2個体がともに親の2個体より悪かった場合
         # 親2個体のうち良かった方のみが局所集団に戻り、局所集団数は1減少する。
-        population.append(p_max)
-    elif p_max.calc_distance() >= c_max.calc_distance() and p_min.calc_distance() <= c_max.calc_distance():
+        population.append(p_good)
+    elif p_good.calc_distance() <= c_good.calc_distance() and p_bad.calc_distance() >= c_good.calc_distance():
         # 親2個体のうちどちらか一方のみが子2個体より良かった場合
         # 親2個体のうち良かった方と子2個体のうち良かった方が局所集団に戻り、局所集団数は変化しない。
-        population.append(c_max)
-        population.append(p_max)
-    elif c_max.calc_distance() >= p_max.calc_distance() and c_min.calc_distance() <= p_max.calc_distance():
+        population.append(c_good)
+        population.append(p_good)
+    elif c_good.calc_distance() <= p_good.calc_distance() and c_bad.calc_distance() >= p_good.calc_distance():
         # 子2個体のうちどちらか一方のみが親2個体より良かった場合
         # 子2個体のうち良かった方のみが局所集団に戻り、全探索空間からランダムに1個体選んで局所集団に追加する。局所集団数は変化しない。
-        population.append(c_max)
+        population.append(c_good)
         population.append(Route())
     else:
         raise ValueError("not comming")
-    
-        
 
 
-# citiesにCityオブジェクトを入れる
+# 時間計測開始
+start = time.perf_counter()
+
+
+# citiesに読み込んだ座標を持つCityオブジェクトを入れる
 for i in range(CITIES_N):
     cities.append(City(cities_data[i][0],
                        cities_data[i][1],
@@ -182,13 +192,11 @@ for i in range(CITIES_N):
 # populationに個体を追加
 for i in range(2):
     population.append(Route())
+    
 
-
-generation = 0
-best_route = random.choice(population)  # 個体(経路)
-best_dist = best_route.calc_distance()  # 距離
-first = best_dist  # 1番優秀
-
+best = random.choice(population)  # 個体(経路)
+record_distance = best.calc_distance()  # 距離
+gen = 0  # 世代カウント用
 
 
 with open('PfGA_result.csv','w') as fout:
@@ -197,23 +205,32 @@ with open('PfGA_result.csv','w') as fout:
     result = []
     
     while True:
+        print(record_distance)
+        population.sort(key=Route.calc_distance)
+        distance1 = population[0].calc_distance()  # 最短経路
+        
+        
+        if distance1 < record_distance:
+            record_distance = distance1
+            best = population[0]  # 最短経路を更新
+            
+            
         pfga()
         
-        population.sort(key=Route.calc_distance)
-        dist1 = population[0].calc_distance()
         
-        if dist1 < best_dist:
-            best_dist = dist1
-            best_route = population[0]
-        
-        generation += 1
-        
-        if generation == 1 or generation%100 == 0:
+        gen+=1
+        if gen == 1 or gen%100 == 0:
             data = []
-            data.extend([generation,best_dist])
+            data.extend([record_distance])
             result.append(data)
-            if generation == 500:
+            if gen == 50000:
                 csvout.writerows(result)
-                print(best_route.citynums)
+                print(best.citynums)
+                
+                
+                # 時間計測終了(秒)
+                end = time.perf_counter()
+                tim = end - start
+                print(tim)
                 break
 
