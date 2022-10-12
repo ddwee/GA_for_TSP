64f47eaf6c5584e7971b8daf0dcf4efae5e9a471
ddwee <103949785+ddwee@users.noreply.github.com>
2022-10-12 18:31:56 +0900

    非同期処理ができてなかったのを修正できたと思う
    218,219行を削除、

--- multiple_PfGA.py
+++ multiple_PfGA.py
@@ -80,38 +80,23 @@ class Route:
         return self.distance
 
 
-async def give2calc_dist(Route_obj):
-    return Route_obj.calc_distance()
-
-
 async def copy_route(route):
     return copy.deepcopy(route)
 
 
-async def mutate(c1,c2):
-    if random.random() > 0.5:
-        select_num = [i for i in range(len(c1.citynums))]
-        select_index = random.sample(select_num, 2)
-        
-        a = c1.citynums[select_index[0]]
-        b = c1.citynums[select_index[1]]
-        c1.citynums[select_index[1]] = a
-        c1.citynums[select_index[0]] = b
-
-    else:
-        select_num = [i for i in range(len(c2.citynums))]
-        select_index = random.sample(select_num, 2)
-        
-        a = c2.citynums[select_index[0]]
-        b = c2.citynums[select_index[1]]
-        c2.citynums[select_index[1]] = a
-        c2.citynums[select_index[0]] = b
-
-    return c1,c2
+async def mutate(ind,num=random.randint(1,2)):
+    indices = random.sample(list(range(CITIES_N)),num)
+    c = Route()
+    c.citynums = ind.citynums[:]
+    
+    # citynumsの2つの値を入れ替える  a,b = b,a
+    for i in range(num-1):
+        c.citynums[indices[i]],c.citynums[indices[(i+1)%num]] = \
+        c.citynums[indices[(i+1)%num]], c.citynums[indices[i]] #↑継続文字
+    return c
 
 
-
-async def crossover(p1, p2):
+async def crossover_2(p1, p2):
     # 子の遺伝子情報
     c1 = await copy_route(p1)
     c2 = await copy_route(p2)
@@ -122,116 +107,209 @@ async def crossover(p1, p2):
     fragment_c1 = c1.citynums[:index]
     fragment_c2 = c2.citynums[:index]
     
+    # 経路を反転(突然変異)
+    if random.random() < 0.001:
+        fragment_c1 = c1.citynums[::-1]
+        fragment_c2 = c2.citynums[::-1]
+    
     notinslice_c1 = [X for X in fragment_c2 if X not in c1.citynums]
     notinslice_c2 = [X for X in fragment_c1 if X not in c2.citynums]
     #リストを合体
     c1.citynums += notinslice_c1
     c2.citynums += notinslice_c2
     
-    mutated_c1, mutated_c2 = await mutate(c1,c2)
+    if random.random() > 0.5:
+        c1 = await mutate(c1)
+    else:
+        c2 = await mutate(c2)
     
-    return mutated_c1,mutated_c2
+    return c1,c2
+
+
+async def crossover_1(p1):
+    # 子の遺伝子情報
+    c1 = await copy_route(p1)
+    c2 = Route()
+    # 切り離す位置をランダムに選択
+    index = random.randint(1,len(cities_data)-2)
+    # indexの前までは自身の経路
+    #indexの後からは相方のリスト(index前の都市と重複しないように)
+    fragment_c1 = c1.citynums[:index]
+    fragment_c2 = c2.citynums[:index]
+    
+    # 経路を反転(突然変異)
+    if random.random() < 0.001:
+        fragment_c1 = c1.citynums[::-1]
+        fragment_c2 = c2.citynums[::-1]
+    
+    notinslice_c1 = [X for X in fragment_c2 if X not in c1.citynums]
+    notinslice_c2 = [X for X in fragment_c1 if X not in c2.citynums]
+    #リストを合体
+    c1.citynums += notinslice_c1
+    c2.citynums += notinslice_c2
+    
+    if random.random() > 0.5:
+        c1 = await mutate(c1)
+    else:
+        c2 = await mutate(c2)
+    
+    return c1,c2
+
+
+async def crossover_0():
+    # 子の遺伝子情報
+    c1 = Route()
+    c2 = Route()
+    # 切り離す位置をランダムに選択
+    index = random.randint(1,len(cities_data)-2)
+    # indexの前までは自身の経路
+    #indexの後からは相方のリスト(index前の都市と重複しないように)
+    fragment_c1 = c1.citynums[:index]
+    fragment_c2 = c2.citynums[:index]
+    
+    # 経路を反転(突然変異)
+    if random.random() < 0.001:
+        fragment_c1 = c1.citynums[::-1]
+        fragment_c2 = c2.citynums[::-1]
+    
+    notinslice_c1 = [X for X in fragment_c2 if X not in c1.citynums]
+    notinslice_c2 = [X for X in fragment_c1 if X not in c2.citynums]
+    #リストを合体
+    c1.citynums += notinslice_c1
+    c2.citynums += notinslice_c2
+    
+    if random.random() > 0.5:
+        c1 = await mutate(c1)
+    else:
+        c2 = await mutate(c2)
+    
+    return c1,c2
 
 
 async def pfga():
 
-    # 2未満なら追加。これだけだとランダムに2こ取り出す動作でエラー吐く。別途初期集団は作っておく
+    # 2未満なら追加。
     if len(population) < 2:
-        population.append(Route())
+        try:
+            individual1, individual2 = await crossover_1(population[0])
+            population.append(individual1)
+            population.append(individual2)
+        except:
+            individual1, individual2 = await crossover_0()
+            population.append(individual1)
+            population.append(individual2)
 
     # ランダムに2個取り出す
     p1 = population.pop(random.randint(0, len(population)-1))
     p2 = population.pop(random.randint(0, len(population)-1))
 
     # 子を作成
-    c1, c2 = await crossover(p1,p2)
+    c1, c2 = await crossover_2(p1,p2)
 
-    if await give2calc_dist(p1) < await give2calc_dist(p2):
+    if p1.calc_distance() < p2.calc_distance():
         p_good = p1  # 短い経路(優秀)
         p_bad = p2  # 長い経路(淘汰される)
     else:
         p_good = p2
         p_bad = p1
-    if await give2calc_dist(c1) < await give2calc_dist(c2):
+    if c1.calc_distance() < c2.calc_distance():
         c_good = c1
         c_bad = c2
     else:
         c_good = c2
         c_bad = c1
 
-    if await give2calc_dist(c_bad) <= await give2calc_dist(p_good):
+    if c_bad.calc_distance() <= p_good.calc_distance():
         # 子2個体がともに親の2個体より良かった場合
         # 子2個体及び適応度の良かった方の親個体計3個体が局所集団に戻り、局所集団数は1増加する。
         population.append(c1)
         population.append(c2)
         population.append(p_good)
-    elif await give2calc_dist(p_bad) <= await give2calc_dist(c_good):
+    elif p_bad.calc_distance() <= c_good.calc_distance():
         # 子2個体がともに親の2個体より悪かった場合
         # 親2個体のうち良かった方のみが局所集団に戻り、局所集団数は1減少する。
         population.append(p_good)
-    elif await give2calc_dist(p_good) <= await give2calc_dist(c_good) and await give2calc_dist(p_bad) >= await give2calc_dist(c_good):
+    elif p_good.calc_distance() <= c_good.calc_distance() and p_bad.calc_distance() >= c_good.calc_distance():
         # 親2個体のうちどちらか一方のみが子2個体より良かった場合
         # 親2個体のうち良かった方と子2個体のうち良かった方が局所集団に戻り、局所集団数は変化しない。
         population.append(c_good)
         population.append(p_good)
-    elif await give2calc_dist(c_good) <= await give2calc_dist(p_good) and await give2calc_dist(c_bad) >= await give2calc_dist(p_good):
+    elif c_good.calc_distance() <= p_good.calc_distance() and c_bad.calc_distance() >= p_good.calc_distance():
         # 子2個体のうちどちらか一方のみが親2個体より良かった場合
-        # 子2個体のうち良かった方のみが局所集団に戻り、全探索空間からランダムに1個体選んで局所集団に追加する。局所集団数は変化しない。
+        # 子2個体のうち良かった方のみが局所集団に戻る
         population.append(c_good)
         population.append(Route())
+        #await pfga()
     else:
         raise ValueError("not comming")
 
 
 async def main():
-    # citiesに読み込んだ座標を持つCityオブジェクトを入れる
-    for i in range(CITIES_N):
-        cities.append(City(cities_data[i][0],
-                           cities_data[i][1],
-                           cities_data[i][2]))  # num,X,Yの順
-        
-    # populationに個体を追加
-    for i in range(2):
-        population.append(Route())
-    
-    best = random.choice(population)  # 個体(経路)
-    record_distance = await give2calc_dist(best)  # 距離
-    
-    with open('asyncio_PfGA_result.csv','w') as fout:
-        
-        csvout = csv.writer(fout)
-        result = []
-        
-        for i in range(50001):
-            print(record_distance)
-            population.sort(key=Route.calc_distance)
-            distance1 = await give2calc_dist(population[0])  # 最短経路
-            
-            if distance1 < record_distance:
-                record_distance = distance1
-                best = population[0]  # 最短経路を更新
-                
-            coro_obj = pfga()
-            loop = asyncio.get_event_loop()
-            task = loop.create_task(coro_obj)
-            
-            await task
-            #await task2
-            
-            if i == 1 or i%100 == 0:
-                data = []
-                data.extend([record_distance])
-                result.append(data)
-                if i == 50000:
-                    csvout.writerows(result)
-                    print(best.citynums)
-                    
+    await asyncio.gather(
+        pfga(),
+        pfga(),
+        pfga(),
+        pfga(),
+        )
 
-start = time.time()
-asyncio.run(main())
-end = time.time()
-print(end-start)
+
+# 時間計測開始
+start = time.perf_counter()
+
+
+# citiesに読み込んだ座標を持つCityオブジェクトを入れる
+for i in range(CITIES_N):
+    cities.append(City(cities_data[i][0],
+                       cities_data[i][1],
+                       cities_data[i][2]))  # num,X,Yの順
     
+# populationに個体を追加
+for i in range(2):
+    population.append(Route())
+
+best = random.choice(population)  # 個体(経路)
+record_distance = best.calc_distance()  # 距離
+gen = 0  # 世代カウント用
+a280 = 2586.76964756316  # a280の最適解
+att48 = 33523.7085074355  # att48の最適解
+
+
+
+
+
+
+with open('multiple_PfGA_result.csv','w') as fout:
+    
+    csvout = csv.writer(fout)
+    result = []
+    
+    while True:
+        population.sort(key=Route.calc_distance)
+        distance1 = population[0].calc_distance()  # 最短経路
+        population = population[:4]  # 最長経路は淘汰
+        
+        if distance1 < record_distance:
+            record_distance = distance1
+            best = population[0]  # 最短経路を更新
+            print(record_distance)
+            data = []
+            data.extend([gen,record_distance])
+            result.append(data)
+        
+        asyncio.run(main())
+        
+        gen+=1
+        # 終了条件：誤差率が--になるまで
+        if (record_distance - a280)/record_distance <= 0.65:
+            csvout.writerows(result)
+            print(best.citynums)
+            # 時間計測終了(秒)
+            end = time.perf_counter()
+            tim = end - start
+            print(tim)
+            break
+
+
 
     
             
